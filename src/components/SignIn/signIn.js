7da4add270473cb9d48bc6cb3dfaacd5cf4976f1
eman-elaziz <eman.elaziz88@gmail.com>
2022-08-14 01:05:40 +0200

    login form

--- src/components/SignIn/signIn.js
+++ src/components/SignIn/signIn.js
@@ -6,9 +6,10 @@ return(
     <section className={`${styles.login}`}>
 <div className={styles.container}>
     <div className={styles.slider}>
-jjjj
+
     </div>
     <div className={styles.loginForm}>
+        
         <div className={styles.loginContent}>
             <div className={styles.logoContainer}>
             <img src='logo.png ' className={styles.logo}/>
@@ -19,12 +20,29 @@ jjjj
             <label className={styles.label}>Login</label>
             <input type="text" className={styles.formControl} placeholder="Email or phone Number"></input>
             <label className={styles.label}>Password</label>
-            <input type="password" className={styles.formControl} placeholder="Enter password"></input>
+            <div style={{position:'relative'}}>
+                   <input type="password" className={styles.formControl} placeholder="Enter password"></input>
+            <img src='show.png' className={styles.show}/></div>
+         <div className={styles.forget}>
+            <div>  <input type="checkbox" className={styles.checkbox} ></input>
+            <label>Remember me</label></div>
+          
+            <a href="#" className={styles.forgetPassword}>Forget password ?</a>
+         </div>
+         <button type="submit" className={styles.signButton}>Sign in</button>
+         <hr className={styles.line}/>
+         <button type="submit" className={`${styles.googleButton} ${styles.signButton}` }>
+         or sign in with google</button>
+<div style={{marginTop:10,textAlign:"center"}}> <span>Dont have an account?</span> 
+<a href='#' style={{textDecoration:"none",paddingLeft:10}}>Sign up now</a></div>
            </form>
-
+<div>fgf</div>
         </div>
+        
         </div>
+       
 </div>
+
     </section>
 
 )
